3fbbf03ecf24554719e64f5e6481582e0cbc48fb
alejandrocardenas-u <alejandro.cardenas.g@ucb.edu.bo>
2025-08-24 17:43:40 -0400

    Add files via upload

---
+++ PRACTICA_01/Ejercicio_01_01.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hola, soy un estudiante de programacion" <<endl;
+    cout << "Alejandro Cardenas Huayta" << endl;
+    cout << "Ingenieria Biomedica" <<endl;
+
+    return 0;
+}
